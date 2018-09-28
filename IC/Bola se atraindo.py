427dec295b5423543ef9e96de50e907e6736120f
Allison Matheus <allisonmatheus.gomes@gmail.com>
2018-09-28 19:20:40 -0300

    Mudanças basicas sujeito a serem modficadas no codigo

--- IC/Bola se atraindo.py
+++ IC/Bola se atraindo.py
@@ -19,12 +19,12 @@ c2 = [pos_w, pos_z]
 sqrt = math.sqrt
 deltax = (pos_x - pos_w)
 deltay = (pos_y - pos_z)
-u = []    
 
-def grav():
+def grav(deltax, deltay):
    i = sqrt((deltax)**2 + (deltay)**2)
    u = [(deltax/i) , (deltay/i)]
-   
+
+       
    
 fundo = pg.display.set_mode((largura, altura))
 pg.display.set_caption("bolinha se mexendo, ou quase")
@@ -35,9 +35,13 @@ while mexer:
         if event.type == pg.QUIT:
             mexer = False   
     fundo.fill(preto)        
-    pg.draw.ellipse(fundo, laranja, [pos_x, pos_y, 40, 40])
-    pg.draw.ellipse(fundo, laranja, [pos_w, pos_z, 40, 40])
+    corpo1 = pg.draw.ellipse(fundo, laranja, [pos_x, pos_y, 40, 40])
+    corpo2 = pg.draw.ellipse(fundo, laranja, [pos_w, pos_z, 40, 40])
     pg.display.update()
+    corpo1 += pos_x + grav(u)
+    
+        
+    
    
 
     
