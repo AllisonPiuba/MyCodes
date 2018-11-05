3bae160d72b193c36acffec503da39dbd3bdc94d
Ygor Canalli <ygor.canalli@gmail.com>
2018-11-05 16:06:31 -0200

    Update Bola se atraindo.py

--- IC/Bola se atraindo.py
+++ IC/Bola se atraindo.py
@@ -1,6 +1,7 @@
 import pygame as pg
 from random import randrange
 import math
+
 laranja=(255,140,0)	
 preto=(0,0,0)
 try:
@@ -8,42 +9,71 @@ try:
 except:
     print("Deu ruim ai irmao")
     
+
+def grav(deltax, deltay):
+   i = math.sqrt((deltax)**2 + (deltay)**2)
+   u = [(deltax/i) , (deltay/i)]
+   
+   return u
+    
 largura = 640
 altura = 400
+tamanho=5
+linha=  1
 pos_x=randrange(5, 640, 1)
 pos_y=randrange(5, 400, 1)
 pos_w=randrange(5, 640, 1)
 pos_z=randrange(5, 400, 1)
-c1 = [pos_x, pos_y]
-c2 = [pos_w, pos_z]
-sqrt = math.sqrt
-deltax = (pos_x - pos_w)
-deltay = (pos_y - pos_z)
 
-def grav(deltax, deltay):
-   i = sqrt((deltax)**2 + (deltay)**2)
-   u = [(deltax/i) , (deltay/i)]
-
-       
-   
 fundo = pg.display.set_mode((largura, altura))
 pg.display.set_caption("bolinha se mexendo, ou quase")
+
+
 mexer = True
 opa = False
 while mexer:
-    for event in pg.event.get():
-        if event.type == pg.QUIT:
-            mexer = False   
-    fundo.fill(preto)        
-    corpo1 = pg.draw.ellipse(fundo, laranja, [pos_x, pos_y, 40, 40])
-    corpo2 = pg.draw.ellipse(fundo, laranja, [pos_w, pos_z, 40, 40])
+    #for event in pg.event.get():
+    #    if event.type == pg.QUIT:
+    #        mexer = False   
+    fundo.fill(preto)       
+    #pg.draw.rect(fundo,laranja,[pos_x, pos_y,tamanho,tamanho])
+    bolota1 = pg.draw.circle(fundo, laranja, [pos_x, pos_y], tamanho)
+    bolota2 = pg.draw.circle(fundo, laranja, [pos_w, pos_z], tamanho)
+    
+    deltax = (pos_x - pos_w)
+    deltay = (pos_y - pos_z)
+    
     pg.display.update()
-    corpo1 += pos_x + grav(u)
     
+    u = grav(deltax, deltay)
+    
+    pos_x -= int(u[0] * 100)
+    pos_y -= int(u[1] * 100)
+    
+    pos_w += int(u[0] * 100)
+    pos_z += int(u[1] * 100)
+
+
+    if pos_x == largura:
+        pos_x += -640
+    if pos_y == altura:
+        pos_y += -400
         
-    
-   
+    pg.time.wait(1000)
+        
+        ##Os coments abaixo sao um problema a ser resolvido para conseguir 
+        #fazer a bola ir e voltar em loop 
+        
+        #mexer = False
+        #opa = True
+#while opa:
+    #for event in pg.event.get():
+        #if event.type == pg.QUIT:
+            #opa = False   
+    #fundo.fill(preto)       
+    #pg.draw.rect(fundo,laranja,[pos_x, pos_y,tamanho,tamanho])
+   # pg.draw.circle(fundo, laranja, [pos_x, pos_y], tamanho)
+    #pg.display.update()
+    #pos_x+=-1
 
-    
 pg.quit()
-
